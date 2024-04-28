c2a146b86bf90f60cbc592671c376497d09ecbca
muhammad-2002 <masumbilla10104@gmail.com>
2024-04-29 03:05:13 +0600

    put route add

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.iwngqer.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,11 +27,99 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
    
-    await client.connect();
-    app.post('/items',(req,res)=>{
-        console.log(req.body)
-        res.send(req.body)
-    })
+    // await client.connect();
+    const database = client.db("Brush_Stokes");
+    const item = database.collection("Craft_Item");
+
+    app.get('/items', async (req, res) => {
+      try {
+          const cursor = item.find();
+          const result = await cursor.toArray();
+          res.send(result);
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+  
+  app.get('/items/:id', async (req, res) => {
+      try {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const cursor = await item.findOne(query);
+          res.send(cursor);
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+  app.delete('/items/:id',async(req,res)=>{
+    try{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await item.deleteOne(query);
+      res.send(result)
+
+
+    }catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+  }
+  })
+  app.put('/items-update/:id',async(req,res)=>{
+    try{
+      const id = req.params.id
+    const filter = { _id: new ObjectId(id) };
+    const options = { upsert: true };
+    const updateCraft = req.body
+    const updateDoc ={
+      $set:{
+      ItemName:updateCraft.ItemName,
+      subcategoryName:updateCraft.subcategoryName,
+      description:updateCraft.description,
+      price:updateCraft.price,
+      stock:updateCraft.stock,
+      customization:updateCraft.customization,
+      image:updateCraft.image,
+      rating:updateCraft.rating,
+      processing_time:updateCraft.processing_time,
+      }
+    }
+    const result = await item.updateOne(filter, updateDoc,options);
+    res.send(result)
+    }catch (error) {
+      console.error(error);
+      res.status(500).send("Internal Server Error");
+  }
+  })
+  
+  app.get('/items-by-email/:email', async (req, res) => {
+      try {
+          const email = req.params.email;
+          const query = { email: email };
+          const cursor = await item.find(query).toArray();
+          if (cursor.length > 0) {
+              res.send(cursor);
+          } else {
+              res.status(404).send("No items found for the provided email address");
+          }
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+  
+  app.post('/items', async (req, res) => {
+      try {
+          const items = req.body;
+          const result = await item.insertOne(items);
+          res.send(result);
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+  
   
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
