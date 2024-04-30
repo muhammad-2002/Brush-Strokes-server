b8b0d56645bf0bd4b732b41ec288c04ba1ad41e2
muhammad-2002 <masumbilla10104@gmail.com>
2024-04-30 19:30:42 +0600

    code organiged and set comment

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const cors =require('cors')
 require('dotenv').config()
 const port = process.env.PORT || 3000
 const  app = express();
-
-
-
 //middleware 
 
 app.use(cors())
@@ -31,14 +28,13 @@ async function run() {
     const database = client.db("Brush_Stokes");
     const item = database.collection("Craft_Item");
     const catagories = database.collection("catagories");
-
-
+//get route
 app.get('/catagories',async(req,res)=>{
   const cursor = catagories.find()
   const result = await cursor.toArray()
   res.send(result)
 })
-    app.get('/items', async (req, res) => {
+app.get('/items', async (req, res) => {
       try {
           const cursor = item.find();
           const result = await cursor.toArray();
@@ -48,31 +44,6 @@ app.get('/catagories',async(req,res)=>{
           res.status(500).send("Internal Server Error");
       }
   });
-  
-  
-  app.get('/items/:id', async (req, res) => {
-      try {
-          const id = req.params.id;
-          const query = { _id: new ObjectId(id) };
-          const cursor = await item.findOne(query);
-          res.send(cursor);
-      } catch (error) {
-          console.error(error);
-          res.status(500).send("Internal Server Error");
-      }
-  });
-  app.delete('/items/:id',async(req,res)=>{
-    try{
-      const id = req.params.id
-      const query = { _id: new ObjectId(id) };
-      const result = await item.deleteOne(query);
-      res.send(result)
-    }catch (error) {
-      console.error(error);
-      res.status(500).send("Internal Server Error");
-  }
-  })
-
   
   app.get('/items-for-craft/:id',async(req,res)=>{
     const id = req.params.id
@@ -87,6 +58,96 @@ app.get('/catagories',async(req,res)=>{
     res.send(result)
 
   })
+  app.get('/ietm-by-Sub-catagory/:sub_cataogry',async(req,res)=>{
+    const sub_category = req.params.sub_cataogry
+    const query = { 
+      subcategoryName: sub_category };
+      const cursor = await item.find(query).toArray()
+      res.send(cursor)
+  })
+  app.get('/items-by-email/:email', async (req, res) => {
+      try {
+          const email = req.params.email;
+          const query = { email: email };
+          const cursor = await item.find(query).toArray();
+          if (cursor.length > 0) {
+              res.send(cursor);
+          } else {
+              res.status(404).send("No items found for the provided email address");
+          }
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+  
+app.get('/items/:id', async (req, res) => {
+      try {
+          const id = req.params.id;
+          const query = { _id: new ObjectId(id) };
+          const cursor = await item.findOne(query);
+          res.send(cursor);
+      } catch (error) {
+          console.error(error);
+          res.status(500).send("Internal Server Error");
+      }
+  });
+
+//post route
+  app.post('/items', async (req, res) => {
+    try {
+        const items = req.body;
+        const result = await item.insertOne(items);
+        res.send(result);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+//put route
+app.put('/items-update/:id',async(req,res)=>{
+  try{
+    const id = req.params.id
+  const filter = { _id: new ObjectId(id) };
+  const options = { upsert: true };
+  const updateCraft = req.body
+  const updateDoc ={
+    $set:{
+    ItemName:updateCraft.ItemName,
+    subcategoryName:updateCraft.subcategoryName,
+    description:updateCraft.description,
+    price:updateCraft.price,
+    stock:updateCraft.stock,
+    customization:updateCraft.customization,
+    image:updateCraft.image,
+    rating:updateCraft.rating,
+    processing_time:updateCraft.processing_time,
+    }
+  }
+  const result = await item.updateOne(filter, updateDoc,options);
+  res.send(result)
+  }catch (error) {
+    console.error(error);
+    res.status(500).send("Internal Server Error");
+}
+})
+//delete route
+
+app.delete('/items/:id',async(req,res)=>{
+    try{
+      const id = req.params.id
+      const query = { _id: new ObjectId(id) };
+      const result = await item.deleteOne(query);
+      res.send(result)
+    }catch (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+  }
+  })
+
+  
+  
   app.put('/items-update/:id',async(req,res)=>{
     try{
       const id = req.params.id
@@ -113,39 +174,7 @@ app.get('/catagories',async(req,res)=>{
       res.status(500).send("Internal Server Error");
   }
   })
-  app.get('/ietm-by-Sub-catagory/:sub_cataogry',async(req,res)=>{
-    const sub_category = req.params.sub_cataogry
-    const query = { 
-      subcategoryName: sub_category };
-      const cursor = await item.find(query).toArray()
-      res.send(cursor)
-  })
-  app.get('/items-by-email/:email', async (req, res) => {
-      try {
-          const email = req.params.email;
-          const query = { email: email };
-          const cursor = await item.find(query).toArray();
-          if (cursor.length > 0) {
-              res.send(cursor);
-          } else {
-              res.status(404).send("No items found for the provided email address");
-          }
-      } catch (error) {
-          console.error(error);
-          res.status(500).send("Internal Server Error");
-      }
-  });
   
-  app.post('/items', async (req, res) => {
-      try {
-          const items = req.body;
-          const result = await item.insertOne(items);
-          res.send(result);
-      } catch (error) {
-          console.error(error);
-          res.status(500).send("Internal Server Error");
-      }
-  });
   
   
     await client.db("admin").command({ ping: 1 });
